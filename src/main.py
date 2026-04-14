3eb2bd86aa600cc02a6b8a17cfb713ded8264437
PBS <jaba7012@proton.me>
2026-04-14 09:23:14 +0200

    Minor changes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import glob
 from pathlib import Path
 
+from server import app
+
 DEVICES_DIR = "/sys/bus/w1/devices/"
 
 
 def find_device():
     device_folder = glob.glob(DEVICES_DIR + "28*")[0]
     device_file = device_folder + "/w1_slave"
+
+    print(f"Device found at `{device_file}`!")
+
+
+if __name__ == "main":
+    app.run("0.0.0.0", 80)
--- src/server.py
+++ src/server.py
@@ -1,8 +1,18 @@
 from flask import Flask
 
-app = Flask("pi-temp")
+app = Flask(__name__)
 
 
 @app.route("/")
 def root():
     return "Hello, world!"
+
+
+@app.route("/temperature")
+def temperature():
+    return "Temperature"
+
+
+@app.route("/raw")
+def raw():
+    return "Raw"
